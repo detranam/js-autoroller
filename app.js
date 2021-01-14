e3e2caf81a11a50a4e4ecb03c2ce4bcbfe508c92
detranam <detranam.code@gmail.com>
2021-01-13 19:52:54 -0600

    Added more functionality

--- app.js
+++ app.js
@@ -12,11 +12,30 @@ class Die {
         return Math.floor((Math.random() * this.sides) + 1);
     }
     rollTwoDropLowest() {
-        var first_roll = Math.floor((Math.random() *this.sides) + 1);
+        var first_roll = Math.floor((Math.random() * this.sides) + 1);
         var second_roll = Math.floor((Math.random() * this.sides) + 1);
         return first_roll > second_roll ? first_roll : second_roll;
     }
 }
+
+var readline = require('readline');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var statNames = [
+    "INT",
+    "REF",
+    "TECH",
+    "COOL",
+    "ATTR",
+    "LUCK",
+    "MA",
+    "BODY",
+    "EMP"
+]
 
 const d10 = new Die(sides = 10);
 //Create an array of size 10 with our correct values in them, to be displayed.
@@ -29,37 +48,30 @@ var characterStats = [
     d10.rollTwoDropLowest(),
     d10.rollTwoDropLowest(),
     d10.rollTwoDropLowest(),
-    d10.rollTwoDropLowest(),
     d10.rollTwoDropLowest()
 ]
 
-characterStats.sort(function(a,b){return b-a});
-
+characterStats.sort(function (a, b) { return b - a });
 console.log(characterStats);
 
+/*
+TODO: I want to give the user the ability to assign their own points, so that
+the 'input' where we ask for their stats can be excluded.
+*/
+
+var response; //EMP value
+rl.question("Enter your EMP value... ", function (answer) {
+    response = answer.trim();
+    rl.close();
+});
 
 
+console.log(response);
+//var EMP = parseInt(response);
+//console.log("Your humanity is %i", EMP*10);
 
 
-
-
-
-
-
-// var readline = require('readline');
-
-// var rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// rl.question("What do you think of node.js? ", function (answer) {
-//     // TODO: Log the answer in a database
-//     console.log("Thank you for your valuable feedback:", answer);
-
+// rl.question("Enter your EMP value: ", function (answer) {
+//     response = answer;
 //     rl.close();
 // });
-
-// //Return a random number between 1 and 10:
-
-// Math.floor((Math.random() * 10) + 1);
