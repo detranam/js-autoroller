92b200a14e75f6055873945903f1a1d298051773
detranam <matthew.detra@gmail.com>
2021-02-10 19:24:14 -0600

    Finished off the csv to json parser

--- tools/sheet.py
+++ tools/sheet.py
@@ -8,15 +8,15 @@ of CSV's and allow it to churn through them creating json files
 out of those dictionaries that were pulled
 '''
 
-
 # Get a list of all files in current directory
 all_files = os.listdir()
+
 # Filter out non *.csv files
 csv_files_to_read = []
 for path in all_files:
     if path[len(path)-4:len(path)] == '.csv':
         csv_files_to_read.append(path)
-        print('CSV [{path}] has been added to \'todo\'')
+        print(f'CSV [{path}] has been added to \'todo\'')
 
 # Create output directory if it doesn't exist
 if(os.path.exists('json_out')):
@@ -27,14 +27,12 @@ else:
 
 # json-ify all the *.csv files into a json file with the same name
 for path in csv_files_to_read:
-    output_path = 'json_out/{path}.json'
+    output_path = f'json_out/{path[:len(path)-4]}.json'
     with open(output_path, 'w') as json_out:
-        with open(path, 'r') as csv_file:
+        with open(path, 'r', encoding='utf8') as csv_file:
             inputdict = csv.DictReader(csv_file)
             this_weapon_type_dict = []
             for row in inputdict:
                 this_weapon_type_dict.append(row)
-            print("JSON.Dumping {path} to {output_path}!")
+            print(f"JSON.Dumping {path} to {output_path}!")
             json.dump(this_weapon_type_dict, json_out)
-
-    
